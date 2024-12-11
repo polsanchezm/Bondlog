14bfdbee9830a81219454ff741412d6902acbcc0
Pol Sanchez <polsamo2003@gmail.com>
2024-12-11 23:10:45 +0100

    Next config updated

--- blog-frontend/next.config.mjs
+++ blog-frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    // Configuración para alias en Next.js
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
 
 export default nextConfig;
