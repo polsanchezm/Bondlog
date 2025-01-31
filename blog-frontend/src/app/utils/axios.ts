51096908234713a9d8c7a62b8ad58392e115cf68
Pol Sanchez <polsamo2003@gmail.com>
2025-01-31 16:12:17 +0100

    Link a la api corregida para render

--- blog-frontend/src/app/utils/axios.ts
+++ blog-frontend/src/app/utils/axios.ts
@@ -1,7 +1,9 @@
 import Axios from "axios";
 
+const localURL = "http://localhost:8000/api/app";
+const renderURL = "https://bondlog.onrender.com/api/app";
 const axios = Axios.create({
-  baseURL: "http://localhost:8000/api/app",
+  baseURL: renderURL,
   headers: {
     "Content-Type": "application/json",
     "X-Requested-With": "XMLHttpRequest",
