c1a82e3af3097ece4467781be29920e26a1d9e31
Pol Sanchez <70636921+polsanchezm@users.noreply.github.com>
2025-02-04 12:52:15 +0100

    ID cambiadas por UUID

--- blog-backend/database/migrations/2024_11_25_144558_create_posts_table.php
+++ blog-backend/database/migrations/2024_11_25_144558_create_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,7 +15,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('subtitle');
             $table->string('body');
-            $table->string('author_id');
+            $table->foreignUuid('author_id')->references('id')->on('users');
             $table->string('author_name');
             $table->timestamps();
         });
