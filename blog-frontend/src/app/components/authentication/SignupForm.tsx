0ad4380795deebfb920d44dfa97eda9a9e29272b
Pol Sanchez <70636921+polsanchezm@users.noreply.github.com>
2025-02-04 20:21:56 +0100

    Formulario corregido

--- blog-frontend/src/app/components/authentication/SignupForm.tsx
+++ blog-frontend/src/app/components/authentication/SignupForm.tsx
@@ -70,19 +70,19 @@ export default function SignupForm() {
             <form className="space-y-4 md:space-y-6" onSubmit={handleRegister}>
               <div>
                 <label
-                  htmlFor="name"
+                  htmlFor="username"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your name
+                  Your username
                 </label>
                 <input
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                   type="name"
-                  name="name"
-                  id="name"
+                  name="username"
+                  id="username"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Jhon"
+                  placeholder="JhonDoe45"
                   required
                 />
               </div>
