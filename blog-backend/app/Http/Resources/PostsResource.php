6a8b3c4e09ba384f4884326295e8f9f23957b312
Pol Sanchez <70636921+polsanchezm@users.noreply.github.com>
2025-02-04 12:49:25 +0100

    Datos añadidos

--- blog-backend/app/Http/Resources/PostsResource.php
+++ blog-backend/app/Http/Resources/PostsResource.php
@@ -15,11 +15,14 @@ class PostsResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'subtitle' => $this->subtitle,
             'body' => $this->body,
             'author_id' => $this->author_id,
             'author_name' => $this->author_name,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
         ];
     }
 }
