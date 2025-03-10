fa7678e5bc9743b9214911b3a73044149b997f68
Pol Sanchez <70636921+polsanchezm@users.noreply.github.com>
2025-03-10 11:28:03 +0100

    Avatar añadido al modelo de usuario

--- blog-backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ blog-backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('username')->unique();
+            $table->string('avatar')->nullable();
             $table->string('role')->default('user');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
